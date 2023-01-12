54c543452eef533c8c234173daf6c2a0bec58741
DannaMish <zachary_sex30@hotmail.com>
2023-01-12 00:21:49 -0500

    Conexión con moviedb api

--- src/Components/MoviesGrid.jsx
+++ src/Components/MoviesGrid.jsx
@@ -1,9 +1,22 @@
-import movies from "./movies.json"
+
 import { MovieCard } from "./MovieCard";
 import style from "./MoviesGrid.module.css"
+import { useEffect } from "react";
+import { useState } from "react";
 
 export function MoviesGrid (){
-   console.log(movies)
+  //let movies = [];
+  const [movies, setMovies] = useState([]);
+ const movieUrl = "https://api.themoviedb.org/3/discover/movie?api_key=5a6341251a6ea4ff8ad19fa7910f516f";
+  useEffect(
+    () => {
+    fetch(movieUrl)
+    .then((result) => result.json())
+    .then((data) => {
+        setMovies(data.results);
+    })
+},[])
+
     return (
         <ul className={style.moviesGrid}>
             {movies.map((movie) => 
@@ -11,4 +24,4 @@ export function MoviesGrid (){
             )}
          </ul>
          );
-        }
+     }
